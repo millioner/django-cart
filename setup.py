f230d8377313f8f9e46e2afa7cadfbc739265480
Arvid Paeglit <fokstrotzustric@arvid-macbook.local>
2011-01-18 16:08:58 +0200

    eggified package

---
+++ setup.py
@@ -0,0 +1,35 @@
+### -*- coding: utf-8 -*- ####################################################
+"""
+Configuration file used by setuptools. It creates 'egg', install all dependencies.
+"""
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+#Dependencies - python eggs
+install_requires = [
+        'setuptools',
+        'Django',
+]
+
+#Execute function to handle setuptools functionality
+setup(name="django-cart",
+    version="0.1",
+    description="Django application to rate objects & stars UI",
+    long_description=read('README'),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    classifiers=(
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+    ),
+)
