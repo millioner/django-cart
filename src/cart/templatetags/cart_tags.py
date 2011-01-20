f9e467663ef3380ae2945cdc8365e91e9b11c1a8
Arvid Paeglit <fokstrotzustric@arvid-macbook.local>
2011-01-20 14:39:39 +0200

    cart_count template tag

--- src/cart/templatetags/cart_tags.py
+++ src/cart/templatetags/cart_tags.py
@@ -5,8 +5,16 @@ from django.core.urlresolvers import reverse
 
 from native_tags.decorators import function
 
+from cart.cart import Cart
+
 @function
 def add_to_cart_url(obj):
     ct_pk = ContentType.objects.get_for_model(obj).pk
     
     return reverse('add_to_cart', content_type_pk=ct_pk, object_pk=obj.pk)
+
+
+def cart_count(request):
+    return Cart(request).cart.item_set.count()
+
+show_cart_count = function(show_cart_count, takes_request=1)
